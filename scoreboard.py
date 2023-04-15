0785621751fb876dc5222e23d658d2f15c90bacc
genelin <genelin825@hotmail.com>
2023-04-15 14:43:39 -0400

    introduced high score

--- scoreboard.py
+++ scoreboard.py
@@ -1,3 +1,5 @@
+import json
+from json.decoder import JSONDecodeError
 from turtle import Turtle
 
 class Scoreboard(Turtle):
@@ -19,9 +21,27 @@ class Scoreboard(Turtle):
         self.write(arg= f"Score: {self.curr_score}", move= False, align = "center", font= ("Arial", 12, "normal"))
 
     def game_over(self):
-        high_score = 10 # to update with actual score from JSON file (this is for testing purpose only)
 
+        high_score = self.get_high_score()
         self.setpos(0, 32)
         self.write(arg= "GAME OVER", move= False, align= "center", font= ("Arial", 40, "bold"))
         self.setpos(0, -35)
         self.write(arg=f'Your Score is: {self.curr_score}\nHigh Score is {high_score}', move=False, align="center", font=("Arial", 25, "normal"))
+
+    def get_high_score(self):
+        with open("highscore.json", "r") as records:
+            try:
+                score_board = json.load(records)
+                if "highScore" in score_board.keys() and score_board["highScore"] >= self.curr_score:
+                    return score_board["highScore"]
+            except JSONDecodeError:
+                pass
+
+        update_score = {
+            "highScore": self.curr_score
+        }
+
+        with open("highscore.json", "w") as records:
+            json.dump(update_score, records)
+            return update_score["highScore"]
+
